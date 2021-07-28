efa5d58b61b9c7a2294c9d8c6e45968f75f41a76
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-28 13:41:27 +0530

    ADDED : User-Id And Created A Rider Repository For Different User

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -1,10 +1,22 @@
 package com.bridgelabz.CabInvoiceGenerator;
 
+import java.util.HashMap;
+
 public class CabInvoiceGenerator {
 
 	private static final int COST_PER_MINUTE = 1;
 	private static final double MINIMUM_COSt_PER_KILOMETER = 10;
 	private static final double MINIMUM_FARE = 5.0;
+	private final HashMap<Integer, double[]> rideRepository = new HashMap<>();
+	private static Integer userId;
+
+	public CabInvoiceGenerator() {
+	}
+
+	public CabInvoiceGenerator(int userId) {
+		CabInvoiceGenerator.userId = userId;
+		rideRepository.put(userId, null);
+	}
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COSt_PER_KILOMETER + time * COST_PER_MINUTE;
@@ -28,7 +40,22 @@ public class CabInvoiceGenerator {
 		for (Ride ride : rides) {
 			totalFare += calculateFare(ride.getDistance(), ride.getTime());
 		}
-
 		return totalFare / rides.length;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void addUserRideRegister(CabInvoiceGenerator user, Ride[] Ride) {
+
+		double[] userRideRegistry = { user.calculateAverageRideCost(Ride), user.calculateTotalFare(Ride),
+				user.getNumberOfRides(Ride) };
+		rideRepository.put(getUserId(), userRideRegistry);
+
+	}
+
+	public double[] getRideDetails(Integer userId) {
+		return rideRepository.get(userId);
 	}
 }
--- src/test/java/com/bridgelabz/CabInvoiceGenerator/TestCabInvoiceGenerator.java
+++ src/test/java/com/bridgelabz/CabInvoiceGenerator/TestCabInvoiceGenerator.java
@@ -1,60 +1,81 @@
 package com.bridgelabz.CabInvoiceGenerator;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCabInvoiceGenerator {
 
-    @Test
-    public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double distance = 2.0;
-        int time = 5;
-        double totalFare = cabInvoiceGenerator.calculateFare(distance,time);
-        System.out.println("Total Fare = " +totalFare);
-        Assert.assertEquals(25, totalFare,0.0);
-    }
+	@Test
+	public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double totalFare = cabInvoiceGenerator.calculateFare(distance, time);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(25, totalFare, 0.0);
+	}
 
-    @Test
-    public void whenGivenLessDistanceOrTimeShouldReturnMinimumFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double distance = 0.1;
-        int time = 1;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
-        System.out.println("Minimum Fare = " +fare);
-        Assert.assertEquals(5, fare,0.0);
-    }
+	@Test
+	public void whenGivenLessDistanceOrTimeShouldReturnMinimumFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = cabInvoiceGenerator.calculateFare(distance, time);
+		System.out.println("Minimum Fare = " + fare);
+		Assert.assertEquals(5, fare, 0.0);
+	}
 
-    @Test
-    public void whenGivenMultipleRidesShouldReturnInvoiceSummary() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        Ride[] rides = { new Ride(2.0, 5),
-                         new Ride(0.1, 1)};
-        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
-        System.out.println("Total Fare = "+totalFare);
-        Assert.assertEquals(30, totalFare,0.0);
-    }
+	@Test
+	public void whenGivenMultipleRidesShouldReturnInvoiceSummary() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+		System.out.println("Total Fare = " + totalFare);
+		Assert.assertEquals(30, totalFare, 0.0);
+	}
 
+	@Test
+	public void givenMultipleRidesshouldReturnSizeAndAverageFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 
-    @Test
-    public void givenMultipleRides_shouldReturnSizeAndAverageFare(){
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		Ride[] rides = { new Ride(20.0, 4), new Ride(45.0, 1), new Ride(75.0, 1), new Ride(45.5, 1) };
 
-        Ride[] rides = { new Ride(20.0,4),
-                         new Ride(45.0,1),
-                         new Ride(75.0,1),
-                         new Ride(45.5,1)};
+		double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+		int numberOfRides = cabInvoiceGenerator.getNumberOfRides(rides);
+		double averageTotalFare = cabInvoiceGenerator.calculateAverageRideCost(rides);
 
-        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
-        int numberOfRides = cabInvoiceGenerator.getNumberOfRides(rides);
-        double averageTotalFare = cabInvoiceGenerator.calculateAverageRideCost(rides);
+		System.out.println("Total Fare = " + totalFare);
+		System.out.println("Number of ride = " + numberOfRides);
+		System.out.println("Average Total Fare " + averageTotalFare);
 
-        System.out.println("Total Fare = " +totalFare);
-        System.out.println("Number of ride = " +numberOfRides);
-        System.out.println("Average Total Fare " +averageTotalFare);
+		Assert.assertEquals(1862, totalFare, 0.0);
+		Assert.assertEquals(4, numberOfRides);
+		Assert.assertEquals(465, averageTotalFare, 0.5);
+	}
 
-        Assert.assertEquals(1862,totalFare,0.0);
-        Assert.assertEquals(4,numberOfRides);
-        Assert.assertEquals(465,averageTotalFare,0.5);
-    }
+	@Test
+	public void whenGivenUserIdShouldReturnAllRidersInvoice() {
+		CabInvoiceGenerator firstUserInvoiceGenerator = new CabInvoiceGenerator(151270);
+		CabInvoiceGenerator secondUserInvoiceGenerator = new CabInvoiceGenerator(151271);
+
+		Ride[] firstUserRides = { new Ride(20.0, 4), new Ride(45.0, 1), new Ride(75.0, 1), new Ride(45.5, 1) };
+
+		Ride[] secondUserRides = { new Ride(20.0, 4), new Ride(45.0, 1), new Ride(75.0, 1) };
+
+		firstUserInvoiceGenerator.addUserRideRegister(firstUserInvoiceGenerator, firstUserRides);
+		secondUserInvoiceGenerator.addUserRideRegister(secondUserInvoiceGenerator, secondUserRides);
+
+		double[] firstUserTotalFare = firstUserInvoiceGenerator.getRideDetails(firstUserInvoiceGenerator.getUserId());
+		double[] secondUserTotalFare = secondUserInvoiceGenerator
+				.getRideDetails(secondUserInvoiceGenerator.getUserId());
+
+		System.out.println("First User Total RidesFare : " + Arrays.toString(firstUserTotalFare));
+		System.out.println("Second User Total RidesFare : " + Arrays.toString(secondUserTotalFare));
+
+		Assert.assertEquals("[465.5, 1862.0, 4.0]", Arrays.toString(firstUserTotalFare));
+		Assert.assertEquals("[468.6666666666667, 1406.0, 3.0]", Arrays.toString(secondUserTotalFare));
+
+	}
 }
