e1f3222a8bae78bce21b4d974062f82c4ed5e615
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-28 11:45:14 +0530

    ADDED : Welcome Message

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.CabInvoiceGenerator;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Cab Invoice Generator Program");
+	}
+}
